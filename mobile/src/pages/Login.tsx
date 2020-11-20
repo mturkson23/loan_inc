cfc55f475c1cede086885b6bbb741fdc54bbc383
Ebo Turkson <mturkson23@gmail.com>
2020-11-20 10:30:34 +0000

    update endpoint base url

--- mobile/src/pages/Login.tsx
+++ mobile/src/pages/Login.tsx
@@ -50,7 +50,7 @@ const Login: React.FC = () => {
     setItem('user_phone', phone);
 
     const api = axios.create({
-      baseURL: `http://127.0.0.1:8001`,
+      baseURL: `http://127.0.0.1:8000`,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
